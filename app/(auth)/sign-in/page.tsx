f68b2b81d0f64c9d30bb611ceaf956542431f6a1
Damy <Owoadedamilola8@gmail.com>
2025-11-17 08:51:03 +0100

    implement initial file and folder

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SIGN_IN = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default SIGN_IN
---
+++ app/(auth)/sign-out/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SIGN_OUT = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default SIGN_OUT
